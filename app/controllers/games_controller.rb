ae5d2f37b4adb9f0938efbebc8796c7195fdc1da
Samuel van Eck <samuelvaneck@gmail.com>
2016-04-28 16:20:38 +0200

    added cors

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,11 @@ class GamesController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
     @games = @category.games
+
+    respond_to do |f|
+      f.html
+      f.json { render json: { category: @category, game: @game } }
+    end
   end
 
   def show
