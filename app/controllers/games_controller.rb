ae3e744534eb895a2d43d88972f5294a7e93994c
HashtAggie <a.el.arras@gmail.com>
2016-04-29 15:05:05 +0200

    edit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class GamesController < ApplicationController
 
   def index
@@ -17,24 +16,3 @@ class GamesController < ApplicationController
   end
 
 end
-||||||| merged common ancestors
-=======
-class GamesController < ApplicationController
-
-  def index
-    @category = Category.find(params[:category_id])
-    @games = @category.games
-
-    respond_to do |f|
-      f.html
-      f.json { render json: { category: @category, games: @games } }
-    end
-  end
-
-  def show
-    @category = Category.find(params[:category_id])
-    @game = @category.games.find(params[:id])
-  end
-
-end
->>>>>>> 4838d9ba573b7c5c57e7889c7490afcc965c9789
