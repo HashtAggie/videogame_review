6f526cbed08bef82e0686f41d811abfe0bcf294b
HashtAggie <a.el.arras@gmail.com>
2016-04-29 15:53:39 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ pc_games = Category.create(name: "PC Games")
 xbox_games = Category.create(name: "XBox Games")
 playstation_games = Category.create(name: "Playstation Games")
 board_games = Category.create(name: "Board Games")
-
+nintendo_games = Category.create(name: "Nintendo Games" )
 puts "seeded #{Category.count} categories!"
 
 print "Seeding some games... "
@@ -12,7 +12,15 @@ print "Seeding some games... "
 Game.create(title: "Commandos", category: pc_games)
 Game.create(title: "Settlers", category: pc_games)
 Game.create(title: "Splinter Cell", category: xbox_games)
-Game.create(title: "Mortal Combat", category: playstation_games)
+Game.create(title: "Mortal Kombat", category: playstation_games)
+Game.create(title: "The Legend of Zelda: Ocarina of Time", category: nintendo_games)
 Game.create(title: "Risk", category: board_games)
 
 puts "seeded #{Game.count} games!"
+
+Review.create(game_id:"Commandos", comment: "This game is actually for loser who wants to kill people in virtual warzone")
+Review.create(game_id:"Settlers", comment: "Settlers if fun for old people. Yeah, that means you!")
+Review.create(game_id:"Splinter Cell", comment: "Ubisoft need to fix this shit, because it's awful. ")
+Review.create(game_id:"Mortal Kombat", comment: "Ultimate kickass game with gore! FATALITY")
+Review.create(game_id:"The Legend of Zelda: Ocarina of Time", comment: "ERMAHGERD!!!!!! BEST GAME EVAAAAAAAAAAAAAAAH!!! SERIOUSLY!!!")
+Review.create(game_id:"Commandos", comment: "This game is actually for loser who wants to kill people in virual warzone")
