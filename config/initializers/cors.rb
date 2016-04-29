5308c4ab6ca32b22c4ec8a7c313c49da3c907c8f
HashtAggie <a.el.arras@gmail.com>
2016-04-29 15:10:03 +0200

    second attempt

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class VideogameReviews::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
@@ -17,24 +17,3 @@ class VideogameReviews::Application
     end
   end
 end
-||||||| merged common ancestors
-=======
-class VideogameReviews::Application
-  config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
-    allow do
-      origins 'localhost:4200', '127.0.0.1:4200'
-
-      resource '/cors',
-        headers: :any,
-        methods: [:post],
-        credentials: true,
-        max_age: 0
-
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :delete, :put, :patch, :options, :head],
-        max_age: 0
-    end
-  end
-end
->>>>>>> 4838d9ba573b7c5c57e7889c7490afcc965c9789
